d86b72e874636d65e67204782a7dad317d459e1f
Jacek Sysiak <j4sysiak@gmail.com>
2025-04-30 07:49:00 +0000

    just a first check 2

--- src/main/java/com/example/testgithubcodespaces/DemoApplication.java
+++ src/main/java/com/example/testgithubcodespaces/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.testgithubcodespaces;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.example.testgithubcodespaces")
